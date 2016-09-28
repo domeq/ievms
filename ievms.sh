7468f3bd019953a5b70d53eb31f3b9e49db04131
Wojciech Iskra <wojtek@domeq.net>
2016-09-28 09:45:50 +0200

    Update ievms.sh

--- ievms.sh
+++ ievms.sh
@@ -166,7 +166,7 @@ install_unar() {
     local url="https://bitbucket.org/WAHa_06x36/theunarchiver/get/unar-1.5.zip"
     local archive=`basename "${url}"`
 
-    download "unar" "${url}" "${archive}" "fbf544d1332c481d7d0f4e3433fbe53b"
+    download "unar" "${url}" "${archive}" "1932287921a23b9a0c27a189a9efd8ed"
 
     unzip "${archive}" || fail "Failed to extract ${ievms_home}/${archive} to ${ievms_home}/, unzip command returned error code $?"
 
